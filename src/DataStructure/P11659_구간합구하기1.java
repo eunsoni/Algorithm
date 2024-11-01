042017cd1914d2c58b07984be1ab7c8bc81da46d
eunsoni <esk12704@gmail.com>
2024-11-01 14:33:50 +0900

    feat: P11659

---
+++ src/DataStructure/P11659_구간합구하기1.java
@@ -0,0 +1,27 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11659_구간합구하기1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int suNo = Integer.parseInt(st.nextToken());
+        int quizNo = Integer.parseInt(st.nextToken());
+        int[] S = new int[suNo+1];
+        st = new StringTokenizer(br.readLine());
+        for(int k = 1; k<suNo+1 ; k++){
+            S[k] = Integer.parseInt(st.nextToken()) + S[k-1];
+        }
+
+        for(int k = 0; k<quizNo ; k++){
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            System.out.println(S[j]-S[i-1]);
+        }
+    }
+}
