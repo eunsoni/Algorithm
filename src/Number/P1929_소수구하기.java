86022ad244d4dce068aefed529b2aaf3affdf953
eunsoni <esk12704@gmail.com>
2024-11-01 14:32:15 +0900

    feat: P1929

---
+++ src/Number/P1929_소수구하기.java
@@ -0,0 +1,25 @@
+package Number;
+
+import java.util.Scanner;
+
+public class P1929_소수구하기 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int A[] = new int[N+1];
+        for(int i = 2; i < N+1; i++){
+            A[i] = i;
+        }
+        for(int j = 2; j<= Math.sqrt(N); j++){
+            if(A[j] == 0 ) continue;
+            for(int i = j+j ; i <= N; i=i+j){
+                A[i] = 0;
+            }
+        }
+        for(int i = M; i<=N; i++){
+            if(A[i] == 0) continue;
+            System.out.println(i);
+        }
+    }
+}
