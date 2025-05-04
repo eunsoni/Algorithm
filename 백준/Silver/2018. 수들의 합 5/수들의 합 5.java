4abf91ca2d78bc9df1d2588bac92d004b6c0c48d
eunsoni <135586807+eunsoni@users.noreply.github.com>
2025-05-04 21:39:33 +0900

    [Silver V] Title: 수들의 합 5, Time: 220 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int eindex = 1;
+        int sindex = 1;
+        long sum = 1;
+        int count = 1;
+        while(eindex!=N){
+            if(sum<N){
+                ++eindex;
+                sum+=eindex;
+            } else if (sum>N) {
+                sum-=sindex;
+                ++sindex;
+            }else{
+                count++;
+                ++eindex;
+                sum+=eindex;
+            }
+        }
+        System.out.println(count);
+    }
+}
