f6148533cef9692d123f1424874894b1d2cf5600
Eunseon Kim <135586807+eunsoni@users.noreply.github.com>
2025-09-22 20:55:03 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 632 ms, Memory: 142120 KB -BaekjoonHub

---
+++ Java/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean visited[];
+    static ArrayList<Integer> A[];
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int P = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        A = new ArrayList[P+1];
+        visited = new boolean[P+1];
+
+
+        for(int i = 1 ; i<P+1 ; i++){
+            A[i] = new ArrayList<>();
+            visited[i] = false;
+        }
+
+        for(int i = 0 ; i< L; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
+        }
+        for(int i = 1 ; i< P+1; i++){
+            if(!visited[i]){
+                visited[i] =true;
+                count++;
+                DFS(i);
+            }
+        }
+
+
+        System.out.print(count);
+    }
+    private static void DFS(int i) {
+        for(int e : A[i]){
+            if(!visited[e]){
+                visited[e] = true;
+                DFS(e);
+            };
+        }
+    }
+}
