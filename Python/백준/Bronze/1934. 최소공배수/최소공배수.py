8c2ed4df3ed27d17f94a951193bd0e975abdeb44
Eunseon Kim <135586807+eunsoni@users.noreply.github.com>
2025-09-26 20:01:10 +0900

    [Bronze I] Title: 최소공배수, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a%b)
+
+for i in range(t):
+    a, b = map(int, input().split())
+    result = a * b // gcd(a, b)
+    print(result)
